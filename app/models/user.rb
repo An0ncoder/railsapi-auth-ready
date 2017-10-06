f77a9fd439946e2b395f4e5a50c10768048203cc
An0ncoder <c0d3andc0ff33@gmail.com>
2017-10-06 20:22:42 +0200

    Basic authentication setup

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# app/models/user.rb
+class User < ApplicationRecord
+  # encrypt password
+  has_secure_password
+
+  # Model associations
+  # Validations
+  validates_presence_of :name, :email, :password_digest
+end
